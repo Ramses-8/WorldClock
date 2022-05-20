b8db2c99511f683b53cbb57eaac153365f6f65bb
Carlos Briones <carlosbr-c@herbalife.com>
2022-05-20 09:28:42 -0500

    Adding support to mount builded application

--- api/api.js
+++ api/api.js
@@ -5,6 +5,16 @@ const port = 8888;
 const v1 = require('./controllers/v1');
 const ui = require('swagger-ui-express');
 const swaggerdoc = require('./swagger.json');
+const path = require('path');
+
+const public = path.resolve(__dirname, 'public');
+app.use('/', express.static(public));
+
+const css = path.resolve(public, 'static/css');
+app.use('/static/css', express.static(css));
+
+const js = path.resolve(public, 'static/js');
+app.use('/static/js', express.static(js));
 
 app.use(express.json());
 app.use(cors({
