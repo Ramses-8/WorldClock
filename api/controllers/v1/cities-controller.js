1fcb957cf03b9266995ce1075257e8678b0a6d0e
Ramses-8 <rosenbergram@hotmail.com>
2022-05-20 08:47:49 -0500

    Swagger update

--- api/controllers/v1/cities-controller.js
+++ api/controllers/v1/cities-controller.js
@@ -3,19 +3,19 @@ const cities = require("../../db/entities");
 
 const router = express.Router();
 
-router.get('/',async (req,res) => {
-    const data= await cities.all()
-    res.send(data);    
+router.get('/', async (req, res) => {
+    const data = await cities.all()
+    res.send(data);
 });
 
-router.get('/:id',async(req,res)=>{
-    const data =await cities.get(req.params.id)
-    if(data){
+router.get('/:id', async (req, res) => {
+    const data = await cities.get(req.params.id)
+    if (data) {
         res.status(200).send(data);
-    } else{
-        res.sendStatus(404);
+    } else {
+        res.status(404).send("Id not found");
     }
-    
+
 });
 
 module.exports = router;
